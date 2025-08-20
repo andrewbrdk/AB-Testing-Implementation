8488c43da1082660f1f23f8e9a40c3ac872b8973
andrew <andrew.brdk@gmail.com>
2025-08-20 14:09:26 +0300

    check split independence

--- simulate_visits.py
+++ simulate_visits.py
@@ -12,7 +12,7 @@ CLICK_PROBS = {
     'B': 0.2
 }
 
-MAX_CONCURRENT = 50
+MAX_CONCURRENT = 30
 SEM = asyncio.Semaphore(MAX_CONCURRENT)
 
 async def simulate_visit(browser):
@@ -66,23 +66,56 @@ async def fetch_experiments():
 async def count_exp_visits_clicks(exp_name):
     visits, clicks = Counter(), Counter()
     events = await fetch_events()
-    if events is not None:
-        device_groups = {}
-        for e in events:
-            if e.get("event") == "exp_groups":
-                device_groups[e.get("deviceId")] = e["params"].get(exp_name).get('group')
-        for e in events:
-            group = device_groups.get(e.get('deviceId')) or e.get("exp_group")
-            if e.get("event") == "pageview":
-                visits[group] += 1
-            elif e.get("event") == "button_click":
-                clicks[group] += 1
+    if events is None:
+        return None, None
+    #todo: use pandas
+    device_groups = {}
+    for e in events:
+        if e.get("event") == "exp_groups":
+            device_groups[e.get("deviceId")] = e["params"].get(exp_name).get('group')
+    for e in events:
+        group = device_groups.get(e.get('deviceId')) or e.get("exp_group")
+        if e.get("event") == "pageview":
+            visits[group] += 1
+        elif e.get("event") == "button_click":
+            clicks[group] += 1
     return visits, clicks
 
 def ctr_ci(v, c):
     ctr = (c / v) if v > 0 else None
     ci = 2 * sqrt(ctr * (1 - ctr) / v) if v > 0 else None
     return ctr, ci
+
+async def check_split_independence(exp1, exp2):
+    exps = await fetch_experiments()
+    if exps is None:
+        return
+    exp1_split = exps.get(exp1, {}).get('groups')
+    exp1_split = {k: v / sum(exp1_split.values()) for k, v in exp1_split.items()}
+    exp2_split = exps.get(exp2, {}).get('groups')
+    exp2_split = {k: v / sum(exp2_split.values()) for k, v in exp2_split.items()}
+    expected_split = {}
+    for g1, s1 in exp1_split.items():
+        for g2, s2 in exp2_split.items():
+            expected_split[(g1, g2)] = s1 * s2
+    events = await fetch_events()
+    if events is None:
+        return
+    #todo: use pandas
+    device_groups = {}
+    for e in events:
+        if e.get("event") == "exp_groups":
+            device_groups[e.get("deviceId")] = {
+                exp1: e["params"].get(exp1).get('group'),
+                exp2: e["params"].get(exp2).get('group')
+            }
+    split = Counter()
+    for d in device_groups.values():
+        split[(d[exp1], d[exp2])] += 1
+    total = sum(split.values())
+    print(f"Split Independence {exp1}/{exp2}:")
+    for g in sorted(split):
+        print(f'{g}: {split[g] / total * 100:.2f}%, expected ({expected_split[g] * 100:.2f}%)')
 
 async def main():
     parser = argparse.ArgumentParser(description="Simulate A/B test visits")
@@ -110,21 +143,24 @@ async def main():
         for group in sorted(button_counts):
             part = (button_counts[group] / N) * 100
             print(f"Group {group}: {button_counts[group]} visits ({part:.2f}%)")
+        print("")
     if headline_counts:
         print("Headline Exp Split:")
         for group in sorted(headline_counts):
             part = (headline_counts[group] / N) * 100
             print(f"Group {group}: {headline_counts[group]} visits ({part:.2f}%)")
+        print("")
 
     exp_name = "homepage_button_test"
     visits, clicks = await count_exp_visits_clicks(exp_name)
-    if len(visits) == 0:
+    if visits is None:
         return
-    print(f"Button Exp /events:")
+    print("Button Exp events:")
     for group in sorted(visits | clicks):
         v, c = visits[group], clicks[group]
         ctr, ci = ctr_ci(v, c)
         print(f"Group {group}: {v} visits, {c} clicks, Conv={ctr*100:.2f} +- {ci*100:.2f}%, Exact: {CLICK_PROBS.get(group)*100:.2f}%")
+    print("")
 
     exps = await fetch_experiments()
     if exps is None or len(exps) == 1:
@@ -135,15 +171,15 @@ async def main():
 
     exp_name = "headline_test"
     visits, clicks = await count_exp_visits_clicks(exp_name)
-    print(f"Headline Exp /events:")
+    print("Headline Exp events:")
     for group in sorted(visits | clicks):
         v, c = visits[group], clicks[group]
         ctr, ci = ctr_ci(v, c)
         expected_ctr = sum([normalized[g] * CLICK_PROBS[g] for g in normalized.keys()])
         print(f"Group {group}: {v} visits, {c} clicks, Conv={ctr*100:.2f} +- {ci*100:.2f}%, Expected: {expected_ctr*100:.2f}%")
+    print("")
 
-    #todo: check independence. use events
-    #P(a & b) = p(a) * p(b)
+    await check_split_independence("homepage_button_test", "headline_test")
 
 if __name__ == "__main__":
     asyncio.run(main())
